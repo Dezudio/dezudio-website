7f3b77b1c24b5ebfc4bf4c444f8c7b666bc4381f
Meg Richards <merichar@merichar.org>
2021-08-17 13:51:39 -0400

    Fix Mobile Nav

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import tw from 'twin.macro';
+import React, { useState } from 'react';
+import tw, {styled} from 'twin.macro';
 import { StaticImage } from 'gatsby-plugin-image';
 import HeaderContainer from './containers';
-import Nav, {
-  NavBurger,
-  NavLink,
+import {
+  Link,
   NavLinkActive,
 } from './nav';
 import Wordmark, {
@@ -14,6 +13,7 @@ import Wordmark, {
 
 
 const Header = ({ pageTitle, children }) => {
+  const [menuOpen, toggleMenuOpen] = useState(false);
 
   function NavLinkGenerator(props) {
     const active = props.active;
@@ -41,18 +41,93 @@ const Header = ({ pageTitle, children }) => {
         </HomeLink>
       </Wordmark>
       <Nav>
-        <NavBurger href="#">
-          <StaticImage
-            alt="Menu"
-            loading="eager"
-            src="../../../images/svg/cheeseburger.svg"
-          />
-        </NavBurger>
+
+      <NavWrapper menuOpen={menuOpen}>
+          <NavBun>
+            <NavBurger href="#" menuOpen={menuOpen} onClick={() => toggleMenuOpen(!menuOpen)}>
+              <div/>
+              <div/>
+              <div/>
+            </NavBurger>
+          </NavBun>
+          <NavLinkGenerator to={"/about"} label={"About"} active={pageTitle}/>
+          <NavLinkGenerator to={"/latest"} label={"Latest"} active={pageTitle}/>
+
+    </NavWrapper>
+      </Nav>
+      <FullNav>
         <NavLinkGenerator to={"/about"} label={"About"} active={pageTitle}/>
         <NavLinkGenerator to={"/latest"} label={"Latest"} active={pageTitle}/>
-      </Nav>
+      </FullNav>
     </HeaderContainer>
   );
 };
 
 export default Header;
+
+const FullNav = tw.nav`
+md:col-end-13
+md:col-start-6
+float-right
+invisible md:visible
+hidden md:block
+text-right`;
+
+const Nav = tw.div`
+float-right
+visible md:invisible
+block md:hidden
+text-right`
+
+const NavWrapper = styled.nav`
+visibility: ${({ menuOpen }) =>
+    menuOpen ? "visible" : "hidden"};
+`;
+
+const NavBun = tw.div`
+visible md:invisible
+ml-28
+`
+
+const NavBurger = styled.button`
+  cursor: pointer;
+  visibility: visible;
+  background: transparent;
+  border: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 2rem;
+  z-index: 11;
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ menuOpen }) => (menuOpen ? "#f45d48" : "black")};
+        border-radius: 10px;
+    transform-origin: 1px;
+    transition: opacity 300ms, transform 300ms;
+    :first-of-type {
+      transform: ${({ menuOpen }) =>
+        menuOpen ? "rotate(45deg)" : "rotate(0)"};
+    }
+    :nth-of-type(2) {
+      opacity: ${({ menuOpen }) => (menuOpen ? "0" : "1")};
+      transform: ${({ menuOpen }) =>
+        menuOpen ? "translateX(-20px)" : "translateX(0)"};
+    }
+    :nth-of-type(3) {
+      transform: ${({ menuOpen }) =>
+        menuOpen ? "rotate(-45deg)" : "rotate(0)"};
+    }
+`
+
+const NavLink = styled(Link)([tw`
+hover:font-sans-bold
+ml-6
+mt-6 md:mt-0
+self-end
+text-dezudiorange
+text-right
+block md:inline
+text-nav-sm lg:text-nav-lg xl:text-nav-xl
+`]);
